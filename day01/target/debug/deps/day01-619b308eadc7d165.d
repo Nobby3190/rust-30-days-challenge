18eb522ed087fb37c648f1008922b7b46ee530d2
nobby <zuzu60410@gmail.com>
2024-06-07 16:16:31 +0000

    add gitignore

--- day01/target/debug/deps/day01-619b308eadc7d165.d
+++
@@ -1,5 +0,0 @@
-/home/nobby/rust-30-days-challenge/day01/target/debug/deps/day01-619b308eadc7d165: src/main.rs
-
-/home/nobby/rust-30-days-challenge/day01/target/debug/deps/day01-619b308eadc7d165.d: src/main.rs
-
-src/main.rs:
