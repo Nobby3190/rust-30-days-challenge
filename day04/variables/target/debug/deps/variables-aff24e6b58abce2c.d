8c69e35c89e5bdef987771ef26418ece8567627b
nobby <zuzu60410@gmail.com>
2024-06-14 05:32:08 +0000

    Add day04

---
+++ day04/variables/target/debug/deps/variables-aff24e6b58abce2c.d
@@ -0,0 +1,5 @@
+/home/nobby/rust-30-days-challenge/day04/variables/target/debug/deps/variables-aff24e6b58abce2c: src/main.rs
+
+/home/nobby/rust-30-days-challenge/day04/variables/target/debug/deps/variables-aff24e6b58abce2c.d: src/main.rs
+
+src/main.rs:
---
+++ day04/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/nobby/rust-30-days-challenge/day04/variables/target/debug/variables: /home/nobby/rust-30-days-challenge/day04/variables/src/main.rs
