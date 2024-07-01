0afa295e2952bed774f12440ea48f402b2bafeba
nobby <zuzu60410@gmail.com>
2024-07-01 09:24:59 +0000

    Add change function

--- day05/reference_borrow/src/main.rs
+++ day05/reference_borrow/src/main.rs
@@ -1,11 +1,18 @@
 fn main() {
     let s1 = String::from("hello");
+    let mut s2 = String::from("hello");
 
     let len = calculate_length(&s1);
     println!("The length of '{s1}' is {len}.");
+    change(&mut s2)
 }
 
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world!");
+    println!("{}", some_string)
 }
