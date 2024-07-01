3ca8f37c856454598e8c1399690c28cfedd611c8
nobby <zuzu60410@gmail.com>
2024-07-01 09:06:27 +0000

    Add reference & borrow

---
+++ day05/reference_borrow/target/debug/deps/reference_borrow-6f5a0fbd6689c80a.d
@@ -0,0 +1,5 @@
+/home/nobby/rust-30-days-challenge/day05/reference_borrow/target/debug/deps/reference_borrow-6f5a0fbd6689c80a: src/main.rs
+
+/home/nobby/rust-30-days-challenge/day05/reference_borrow/target/debug/deps/reference_borrow-6f5a0fbd6689c80a.d: src/main.rs
+
+src/main.rs:
---
+++ day05/reference_borrow/target/debug/reference_borrow.d
@@ -0,0 +1,1 @@
+/home/nobby/rust-30-days-challenge/day05/reference_borrow/target/debug/reference_borrow: /home/nobby/rust-30-days-challenge/day05/reference_borrow/src/main.rs
